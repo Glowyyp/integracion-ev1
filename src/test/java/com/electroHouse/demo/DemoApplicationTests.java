6dd8eed96f8632a1b9cf5d016ca365f22caeb24a
Glowy <luislizanadeep@gmail.com>
2025-05-28 20:02:41 -0400

    primer com

---
+++ src/test/java/com/electroHouse/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electroHouse.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
